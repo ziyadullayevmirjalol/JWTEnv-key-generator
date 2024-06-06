76ce3424b7737f31eb1c77d008e34c1c400c55ab
Mirjalol Ziyadullayev <112009351+ziyadullayevmirjalol@users.noreply.github.com>
2024-06-07 02:30:32 +0500

    Add project files.

---
+++ JWT Environment key generator/KeyGenerator.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Cryptography;
+
+public class KeyGenerator
+{
+    public static string GenerateKey()
+    {
+        byte[] keyBytes = new byte[32];
+        using (var rng = new RNGCryptoServiceProvider())
+        {
+            rng.GetBytes(keyBytes);
+        }
+
+        return Convert.ToBase64String(keyBytes);
+    }
+}
---
+++ JWT Environment key generator/Program.cs
@@ -0,0 +1,7 @@
+﻿using System.Text;
+
+string key = KeyGenerator.GenerateKey();
+Console.WriteLine(key);
+
+var result = Encoding.UTF8.GetBytes(key);
+Console.WriteLine("\nLenght:" + key.Length);
